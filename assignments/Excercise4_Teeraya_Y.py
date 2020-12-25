9af011109bcef72b125650f4031f97fb664bcf0a
Teeraya Yawichai <teerayaya@gmail.com>
2020-12-25 19:59:59 +0700

    commit excercise4

---
+++ assignments/Excercise4_Teeraya_Y.py
@@ -0,0 +1,21 @@
+'''ุ
+สร้างตัวแปรเก็บคะแนนในแต่ละวิชาและแสดงผลออกมาในรูปแบบ
+--- Your Score ---
+Foundation English : คะแนนที่ได้
+General Business : คะแนนที่ได้
+Introduction to Computer Systems : คะแนนที่ได้
+Computer Programming : คะแนนที่ได้
+'''
+
+''' Variable section '''
+scoreFoundationEnglish = 56.0
+scoreGenBusiness       = 66.0
+scoreIntroCompSystem   = 76.0
+scoreCompProgramming   = 86.0
+
+''' Display section '''
+print("--- Your Score ---")
+print("Foundation English :", scoreFoundationEnglish)
+print("General Business :", scoreGenBusiness)
+print("Introduction to Computer Systems :",scoreIntroCompSystem)
+print("Computer Programming :", scoreCompProgramming)
