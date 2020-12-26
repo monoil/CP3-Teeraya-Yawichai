65d5aa2e16f6c52a416e6dfb780b2354030e8b6f
Teeraya Yawichai <teerayaya@gmail.com>
2020-12-26 17:51:07 +0700

    submit excercise 5

---
+++ assignments/Excercise5_1_Teeraya_Y.py
@@ -0,0 +1,15 @@
+'''
+    Excercise 5_1
+'''
+
+x = int(input("Please enter a number of X: "))
+y = int(input("Please enter a number of Y: "))
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+
+print(x, "+", y, "=", int(addition))
+print(x, "-", y, "=", int(subtraction))
+print(x, "*", y, "=", int(multiplication))
+print(x, "/", y, "=", int(division))
---
+++ assignments/Excercise5_2_Teeraya_Y.py
@@ -0,0 +1,7 @@
+'''
+    Excercise 5_2
+'''
+distance = int(input("Please enter distance: "))
+time     = int(input("Please enter time: "))
+velocity = int(distance/time)
+print(velocity, "km/h")
