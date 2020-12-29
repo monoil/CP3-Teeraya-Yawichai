20e2007d4dcffce6a8e0f0ea2eead259a827bef4
Teeraya Yawichai <teerayaya@gmail.com>
2020-12-29 15:44:23 +0700

    submit lecture 50

--- assignments/Lecture50_Teeraya_Y.py
+++ assignments/Lecture50_Teeraya_Y.py
@@ -1,14 +1,18 @@
 def addition(x, y):
     print(x,"+",y,"=", x+y)
 
-def subtraction(x,y):
+
+def subtraction(x, y):
     print(x,"-",y,"=", x-y)
 
-def multiplication(x,y):
+
+def multiplication(x, y):
     print(x, "*", y, "=", x*y)
 
-def division(x,y):
+
+def division(x, y):
     print(x, "/", y, "=", x/y)
+
 
 addition(100, 25)
 subtraction(100, 25)
